056f8ef1acc83afa573b72ac758050bbec32107a
Josue Arellano <arellajosue@protonmail.com>
2018-05-04 20:24:22 -0700

    Finished Strassen method. Modified SortingAnalysis.

--- Matrices.java
+++ Matrices.java
@@ -47,9 +47,45 @@ public class Matrices
             int[][] b12 = new int[n / 2][n / 2];
             int[][] b21 = new int[n / 2][n / 2];
             int[][] b22 = new int[n / 2][n / 2];
+            int[][] P = new int[n / 2][n / 2];
+            int[][] Q = new int[n / 2][n / 2];
+            int[][] R = new int[n / 2][n / 2];
+            int[][] S = new int[n / 2][n / 2];
+            int[][] T = new int[n / 2][n / 2];
+            int[][] U = new int[n / 2][n / 2];
+            int[][] V = new int[n / 2][n / 2];
 
-            partition(n / 2, arrA, a11, a12, a21, a22);
-            partition(n / 2, arrB, b11, b12, b21, b22);
+            partition(n, arrA, a11, a12, a21, a22);
+            partition(n, arrB, b11, b12, b21, b22);
+            
+            strassenMatrixMult(n / 2, matrixAdd(n / 2, a11, a22), matrixAdd(n / 2, b11, b22), P);
+            strassenMatrixMult(n / 2, matrixAdd(n / 2, a21, a22), b11, Q);
+            strassenMatrixMult(n / 2, a11, matrixSub(n / 2, b12, b22), R);
+            strassenMatrixMult(n / 2, a22, matrixSub(n / 2, b21, b11), S);
+            strassenMatrixMult(n / 2, matrixAdd(n / 2, a11, a12), b22, T);
+            strassenMatrixMult(n / 2, matrixSub(n / 2, a21, a11), matrixAdd(n / 2, b11, b12), U);
+            strassenMatrixMult(n / 2, matrixSub(n / 2, a12, a22), matrixAdd(n / 2, b21, b22), V);
+            
+            int[][] c11 = matrixSub(n / 2, matrixAdd(n / 2, matrixAdd(n / 2, P, S), V), T);
+            int[][] c12 = matrixAdd(n / 2, R, T);
+            int[][] c21 = matrixAdd(n / 2, Q, S);
+            int[][] c22 = matrixSub(n / 2, matrixAdd(n / 2, matrixAdd(n / 2, P, R), U), Q);
+            
+            matrixConcat(n, arr, c11, c12, c21, c22);
+        }
+    }
+    
+    private static void matrixConcat(int n, int[][] C, int[][] c11, int[][] c12, int[][] c21, int[][] c22)
+    {
+        for(int i = 0; i < n / 2; i++)
+        {
+            for(int j = 0; j < n/2; j++)
+            {
+                C[i][j] = c11[i][j];
+                C[i][j+(n/2)] = c12[i][j];
+                C[i+(n/2)][j] = c21[i][j];
+                C[i+(n/2)][j+(n/2)] = c22[i][j];
+            }
         }
     }
     
@@ -66,8 +102,30 @@ public class Matrices
         return c;
     }
     
+    private static int[][] matrixSub(int n, int[][] a, int[][] b)
+    {
+        int[][] c = new int[n][n];
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = 0; j < n; j++)
+            {
+                c[i][j] = a[i][j] - b[i][j];
+            }
+        }
+        return c;
+    }
+    
     private static void partition(int n, int[][] a, int[][] a11, int[][] a12, int[][] a21, int[][] a22)
     {
-        
+        for(int i = 0; i < n/2; i++)
+        {
+            for(int j = 0; j < n/2; j++)
+            {
+                a11[i][j] = a[i][j];
+                a12[i][j] = a[i][j+(n/2)];
+                a21[i][j] = a[i+(n/2)][j];
+                a22[i][j] = a[i+(n/2)][j+(n/2)];
+            }
+        }
     }
 }
--- SortAnalysis.java
+++ SortAnalysis.java
@@ -3,10 +3,11 @@
  *   File:      SortAnalysis.java
  *   Project:   #1
  *   Due:       Apr 26, 2018
- *   Course:    cs14103-w18
+ *   Course:    cs331-s18
  *
  *   Description:
- *              This
+ *              This program implements and analyzes sorting algorithms and matrix
+ *                  multiplication algorithms.
  */
 package sortanalysis;
 import java.util.Random;
@@ -15,67 +16,104 @@ public class SortAnalysis
 {
     public static void main(String[] args)
     {
+        //Random object to create random integers to populate arrays.
         Random rand = new Random();
+        
+        //This is initialized to try to stop the program from running for too long.
         long timer = System.currentTimeMillis();
+        //Long type variables to find the time it takes to run certain methods.
+        long startTimer;
+        long endTimer;
+        
+        //Title: Sorting Algorithms
         System.out.println("SORTING ALGORITHM TEST");
+        
+        //for loop to create several different size of arrays to sort.
         for(int n = 10000; n < 10000000; n *= 2)
         {
+            //New array of size n.
             int[] a = new int[n];
 
+            //For loop to populate the array with random integers.
             for (int i = 0; i < n; i++)
             {
                 a[i] = rand.nextInt(1000);
             }
+            
+            //Copies of array to be able to sort the same array and compare
+            //  the time it takes for each algorithm to store the same array.
             int[] b = a;
             int[] c = a;
 
-            if (System.currentTimeMillis() > timer * 10 * 60)
+            //This comparison checks to see if the loop has been running for too
+            //  long.
+            if (System.currentTimeMillis() - timer > 5 * 60 * 1000)
             {
                 System.out.println("Too much time has passed.");
                 break;
             }
             
+            //Print the size of the array.
             System.out.printf("Array of size %,d\n", n);
 
-            long exTime = System.currentTimeMillis();
-            Sorting.exchangeSort(c);
-            long exTimeEnd = System.currentTimeMillis() - exTime;
-            System.out.println("\tExchange Sort took " + exTimeEnd + " ms.");
+            startTimer = System.currentTimeMillis();                        //Start timer.
+            Sorting.exchangeSort(c);                                        //Run algorithm.
+            endTimer = System.currentTimeMillis() - startTimer;             //Get runtime by getting the difference of the current time and the Start time.
+            System.out.println("\tExchange Sort took " + endTimer + " ms.");//Display runtime.
 
-            long qTime = System.currentTimeMillis();
+            startTimer = System.currentTimeMillis();
             Sorting.quickSort(0, n, b);
-            long qTimeEnd = System.currentTimeMillis() - qTime;
-            System.out.println("\tQuick Sort took " + qTimeEnd + " ms.");
+            endTimer = System.currentTimeMillis() - startTimer;
+            System.out.println("\tQuick Sort took " + endTimer + " ms.");
 
-            long mTime = System.currentTimeMillis();
+            startTimer = System.currentTimeMillis();
             Sorting.mergeSort(n, a);
-            long mTimeEnd = System.currentTimeMillis() - mTime;
-            System.out.println("\tMerge Sort took " + mTimeEnd + " ms.");
-            
-            System.out.println();
+            endTimer = System.currentTimeMillis() - startTimer;
+            System.out.println("\tMerge Sort took " + endTimer + " ms.\n");
         }
         
+        //Title: Matrix Multiplication Test
         System.out.println("MATRIX MULTIPLICATION TEST");
-        for(int n = 2; n < 1000000; n *= 2)
+        
+        //Reinitialize the timer to find the runtime of the following loop.
+        timer = System.currentTimeMillis();
+        
+        //For loop to run matrix multiplication on multiple sizes of matrices.
+        for(int n = 2; n < 100000000; n *= 2)
         {
+            //Three two dimensional arrays are used to creat
             int[][] a = new int[n][n];
+            int[][] a1 = new int[n][n];
             int[][] b = new int[n][n];
+            int[][] b1 = new int[n][n];
             int[][] c = new int[n][n];
             
-            System.out.printf("%,d X %,d Matrix\n", n, n);
             for(int i = 0; i < n; i++)
             {
                 for(int j = 0; j < n; j++)
                 {
-                    a[i][j] = rand.nextInt(10);
-                    b[i][j] = rand.nextInt(10);
+                    a[i][j] = a1[i][j] = rand.nextInt(100);
+                    b[i][j] = b1[i][j] = rand.nextInt(100);
                 }
             }
+
+            if (System.currentTimeMillis() - timer > 5 * 60 * 1000)
+            {
+                System.out.println("Too much time has passed.");
+                break;
+            }
             
-            long mTime = System.currentTimeMillis();
+            System.out.printf("%,d X %,d Matrix\n", n, n);
+            
+            startTimer = System.currentTimeMillis();
             Matrices.matrixMult(n, a, b, c);
-            long mTimeEnd = System.currentTimeMillis() - mTime;
-            System.out.println("\tOriginal Matrix Mult took " + mTimeEnd + " ms.");
+            endTimer = System.currentTimeMillis() - startTimer;
+            System.out.println("\tOriginal Matrix Mult took " + endTimer + " ms.");
+            
+            startTimer = System.currentTimeMillis();
+            Matrices.strassenMatrixMult(n, a, b, c);
+            endTimer = System.currentTimeMillis() - startTimer;
+            System.out.println("\tStrassen Matrix Mult took " + endTimer + " ms.\n");
         }
     }
     
